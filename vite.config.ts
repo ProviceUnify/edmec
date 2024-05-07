5f2c24c9f215bd4241181608c0a188c68ad454a7
ProviceUnify <ilyapravotkin@gmail.com>
2024-05-07 20:03:10 +0300

    Added 'base' parameter

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import VueDevTools from 'vite-plugin-vue-devtools'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), VueDevTools()],
-
+  base: 'elite-dangerous-material-exchange-calculator',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
