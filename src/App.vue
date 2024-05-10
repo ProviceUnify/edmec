b5af025f69c5083d281d32341c2ee707606f9e23
ProviceUnify <ilyapravotkin@gmail.com>
2024-05-10 22:26:56 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,21 @@
 <script setup lang="ts">
-import MaterialCard from './components/MaterialCard.vue';;
+import { ref } from 'vue';
+import MaterialCard from './components/MaterialCard.vue'
+
+const t = ref(true)
+
+function test() {
+  t.value = !t.value
+}
 </script>
 
 <template>
   <div>
-    <MaterialCard grade="one" />
-    <MaterialCard grade="two" />
-    <MaterialCard grade="three" />
-    <MaterialCard grade="four" />
-    <MaterialCard grade="five" />
+    <MaterialCard grade="one" :is-available-for-exchange="t" id="testElement" />
+    <MaterialCard grade="two" :is-available-for-exchange="t" />
+    <MaterialCard grade="three" :is-available-for-exchange="t" />
+    <MaterialCard grade="four" :is-available-for-exchange="t" />
+    <MaterialCard grade="five" :is-available-for-exchange="true" @click="test" />
   </div>
 </template>
 
