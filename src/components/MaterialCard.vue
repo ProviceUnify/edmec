168becc74a4c871f5a3367f05def7db843adc107
ProviceUnify <ilyapravotkin@gmail.com>
2024-05-10 22:26:45 +0300

    Revision and improvments

--- src/components/MaterialCard.vue
+++ src/components/MaterialCard.vue
@@ -1,47 +1,71 @@
 <template>
-  <div class="material-card elite-text">
+  <div class="elite-text material-card" :class="{ 'material-card-red': !isRed }">
     <div class="material-card-background">
       <div class="material-card-content">
-        <Grade1Icon v-if="$props.grade == GradesEnum.One" class="GradeIcon" />
-        <Grade2Icon v-if="$props.grade == GradesEnum.Two" class="GradeIcon" />
-        <Grade3Icon v-if="$props.grade == GradesEnum.Three" class="GradeIcon" />
-        <Grade4Icon v-if="$props.grade == GradesEnum.Four" class="GradeIcon" />
-        <Grade5Icon v-if="$props.grade == GradesEnum.Five" class="GradeIcon" />
-        35
+        <component :is="icon" class="GradeIcon" />
+        7
       </div>
       <div class="material-card-title">
-        Прототипы теплоизлучателей
+        <span class="material-card-title-content">Прототипы of heat radiators</span>
       </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import Grade1Icon from '@/assets/svg/Grade1Icon.vue';
-import Grade2Icon from '@/assets/svg/Grade2Icon.vue';
-import Grade3Icon from '@/assets/svg/Grade3Icon.vue';
-import Grade4Icon from '@/assets/svg/Grade4Icon.vue';
-import Grade5Icon from '@/assets/svg/Grade5Icon.vue';
-import type { PropType } from 'vue';
+// Import of SVG icons
+import { Grade1Icon, Grade2Icon, Grade3Icon, Grade4Icon, Grade5Icon } from '@/assets/svg';
+import { computed, ref, watch, type PropType } from 'vue'
 
+/**
+ * This `enum` is used for data integrity of variants that could be used for prop `grade`
+ */
 enum GradesEnum {
-  One = 'one',
-  Two = 'two',
-  Three = 'three',
-  Four = 'four',
-  Five = 'five',
+  'one',
+  'two',
+  'three',
+  'four',
+  'five',
 }
+type Grades = keyof typeof GradesEnum
 
-type Grades = 'one' | 'two' | 'three' | 'four' | 'five' | null | undefined
-
+/**
+ * Defined props of this component
+ */
 const props = defineProps({
-  grade: String as PropType<Grades>
+  grade: String as PropType<Grades>,
+  isAvailableForExchange: Boolean
 })
 
-console.log(props)
+/**
+ * Computation of what SVG icon should be used for current MaterialCard
+ */
+const icon = computed(() => {
+  var gradesList = [Grade1Icon, Grade2Icon, Grade3Icon, Grade4Icon, Grade5Icon]
+  if (props.grade == undefined) return Grade1Icon
+  return gradesList[GradesEnum[props.grade]]
+})
+
+/**
+ * Sets red appearance for `MaterialCard` that cannot be exchanged
+ */
+const isRed = ref(props.isAvailableForExchange)
+
+// #region WATCHER ZONE
+
+/**
+ * Watcher of defined prop for `isAvailableForExchange`
+ * @abstract Need for availibity change value from outer elements
+ */
+watch(() => props.isAvailableForExchange, (newValue) => {
+  isRed.value = newValue
+})
+
+// #endregion
 </script>
 
 <style scoped>
+/* Usual appearance */
 .material-card {
   user-select: none;
   border: 2px solid var(--secondary);
@@ -51,23 +75,7 @@ console.log(props)
   color: rgb(var(--primary));
 }
 
-.material-card:hover .material-card-background {
-  background-color: var(--primary-background);
-}
-
-.material-card:hover .material-card-title {
-  background-color: rgb(var(--primary));
-  color: var(--black);
-}
-
-.material-card:hover .material-card-content {
-  color: var(--black);
-}
-
-.material-card:hover .GradeIcon {
-  fill: black;
-}
-
+/* Usual appearance */
 .material-card-background {
   display: grid;
   height: 100%;
@@ -77,6 +85,7 @@ console.log(props)
   background-color: var(--secondary-background);
 }
 
+/* Usual appearance */
 .material-card-content {
   display: grid;
   justify-items: center;
@@ -86,15 +95,74 @@ console.log(props)
   grid-template-rows: auto 35%;
 }
 
-.material-card-content .GradeIcon {
+/* Usual appearance */
+.GradeIcon {
   width: 2.5rem;
   align-self: flex-end;
   margin-bottom: 4px;
 }
 
+/* Usual appearance */
 .material-card-title {
   align-content: center;
   font-size: 10pt;
+  font-weight: bold;
   background-color: var(--secondary);
+}
+
+/* Usual appearance */
+.material-card-title-content {
+  display: inline-block;
+  width: 95%;
+}
+
+/* 'Exchange unavailable' appearance */
+.material-card-red {
+  border: 2px solid var(--secondary-red-background);
+  color: var(--secondary-red-text);
+}
+
+/* 'Exchange unavailable' appearance */
+.material-card-red .GradeIcon {
+  fill: var(--secondary-red-text);
+}
+
+/* 'Exchange unavailable' appearance */
+.material-card-red .material-card-background {
+  background-color: var(--secondary-red-background);
+}
+
+/* 'Exchange unavailable' appearance */
+.material-card-red .material-card-title {
+  background-color: var(--secondary-red);
+}
+
+/* Usual:HOVER appearance */
+.material-card:hover .material-card-background {
+  background-color: var(--primary-background);
+  color: var(--black);
+}
+
+/* Usual:HOVER appearance */
+.material-card:hover .material-card-title {
+  background-color: rgb(var(--primary));
+}
+
+/* Usual:HOVER and 'Exchange unavailable':HOVER appearance */
+.material-card:hover .GradeIcon,
+.material-card-red:hover .GradeIcon {
+  fill: black;
+}
+
+/* 'Exchange unavailable':HOVER appearance */
+.material-card-red:hover .material-card-background {
+  background-color: var(--primary-red-background);
+  color: var(--black);
+}
+
+/* 'Exchange unavailable':HOVER appearance */
+.material-card-red:hover .material-card-title {
+  background-color: var(--primary-red);
+  color: var(--black);
 }
 </style>
