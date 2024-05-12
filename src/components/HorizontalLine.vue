8ba7aec56cdf4363180439041f1fe25fcde43432
ProviceUnify <ilyapravotkin@gmail.com>
2024-05-12 22:34:22 +0300

    Create HorizontalLine.vue
    Horizontal line component

---
+++ src/components/HorizontalLine.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="horizontal-line" />
+</template>
+
+<style scoped>
+.horizontal-line:after {
+    content: " ";
+    display: block;
+    height: 75%;
+    border-bottom: 2px solid var(--secondary);
+}
+</style>
