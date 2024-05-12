a317aca0bc99539bb79806961138ead0bc3a69dd
ProviceUnify <ilyapravotkin@gmail.com>
2024-05-12 18:26:43 +0300

    Update MaterialCard.vue

--- src/components/MaterialCard.vue
+++ src/components/MaterialCard.vue
@@ -1,12 +1,16 @@
 <template>
-  <div class="elite-text material-card" :class="{ 'material-card-red': !isRed }">
+  <div class="elite-text material-card" :class="{ 'material-card-red': !_isRed, 'is-selected': _isSelected }"
+    @click="click">
     <div class="material-card-background">
-      <div class="material-card-content">
-        <component :is="icon" class="GradeIcon" />
-        7
+      <div class="material-card-content-wrapper">
+        <component :is="typeIcon" class="material-type" />
+        <div class="material-card-content">
+          <component :is="gradeIcon" class="grade-icon" :class="{ 'dimmed': _isDimmed }" />
+          <span class="material-card-content-count" :class="{ 'dimmed': _isDimmed }">7</span>
+        </div>
       </div>
       <div class="material-card-title">
-        <span class="material-card-title-content">Прототипы of heat radiators</span>
+        <span class="material-card-title-content" :class="{ 'dimmed': _isDimmed }">Прототипы of heat radiators</span>
       </div>
     </div>
   </div>
@@ -14,7 +18,7 @@
 
 <script setup lang="ts">
 // Import of SVG icons
-import { Grade1Icon, Grade2Icon, Grade3Icon, Grade4Icon, Grade5Icon } from '@/assets/svg';
+import { Grade1Icon, Grade2Icon, Grade3Icon, Grade4Icon, Grade5Icon, RawIcon, EncodedIcon, ManufacturedIcon } from '@/assets/svg'
 import { computed, ref, watch, type PropType } from 'vue'
 
 /**
@@ -30,26 +34,69 @@ enum GradesEnum {
 type Grades = keyof typeof GradesEnum
 
 /**
+ * This `enum` is used for data integrity of variants that could be used for prop `materialType`
+ */
+enum MaterialTypesEnum {
+  'raw',
+  'encoded',
+  'manufactured'
+}
+type Types = keyof typeof MaterialTypesEnum
+
+/**
  * Defined props of this component
  */
 const props = defineProps({
   grade: String as PropType<Grades>,
-  isAvailableForExchange: Boolean
+  materialType: String as PropType<Types>,
+  isRed: Boolean,
+  isDimmed: Boolean
 })
+
+/**
+ * Defined events of this component
+ */
+const emits = defineEmits(['mc-click'])
 
 /**
  * Computation of what SVG icon should be used for current MaterialCard
  */
-const icon = computed(() => {
+const gradeIcon = computed(() => {
   var gradesList = [Grade1Icon, Grade2Icon, Grade3Icon, Grade4Icon, Grade5Icon]
   if (props.grade == undefined) return Grade1Icon
   return gradesList[GradesEnum[props.grade]]
 })
 
 /**
+ * Computation of what SVG material type icon should be used for current MaterialCard
+ */
+const typeIcon = computed(() => {
+  var typesList = [RawIcon, EncodedIcon, ManufacturedIcon]
+  if (props.materialType == undefined) return RawIcon
+  return typesList[MaterialTypesEnum[props.materialType]]
+})
+
+/**
  * Sets red appearance for `MaterialCard` that cannot be exchanged
  */
-const isRed = ref(props.isAvailableForExchange)
+const _isRed = ref(props.isRed)
+
+/**
+ * Sets dimmed appearance for `MaterialCard` that cannot be exchanged as preview on hover
+ */
+const _isDimmed = ref(props.isDimmed)
+
+const _isSelected = ref(false)
+
+// #region METHODS ZONE
+
+function click() {
+  if (!_isRed.value) return
+  _isSelected.value = !_isSelected.value
+  emits('mc-click', _isSelected.value)
+}
+
+// #endregion
 
 // #region WATCHER ZONE
 
@@ -57,14 +104,33 @@ const isRed = ref(props.isAvailableForExchange)
  * Watcher of defined prop for `isAvailableForExchange`
  * @abstract Need for availibity change value from outer elements
  */
-watch(() => props.isAvailableForExchange, (newValue) => {
-  isRed.value = newValue
+watch(() => props.isRed, (newValue) => {
+  _isRed.value = newValue
+})
+
+/**
+ * Watcher of defined prop for `isAvailableForExchangePreview`
+ * @abstract Need for availibity change value from outer elements
+ */
+watch(() => props.isDimmed, (newValue) => {
+  _isDimmed.value = newValue
 })
 
 // #endregion
 </script>
 
 <style scoped>
+/* Dimmed brightness for preview of materials that unavailable for exchange */
+.dimmed {
+  filter: brightness(.55);
+}
+
+/* Disabling of brightness dimming for preview of materials that unavailable for exchange when hover */
+.material-card:hover .dimmed,
+.material-card-red:hover .dimmed {
+  opacity: 1;
+}
+
 /* Usual appearance */
 .material-card {
   user-select: none;
@@ -75,6 +141,16 @@ watch(() => props.isAvailableForExchange, (newValue) => {
   color: rgb(var(--primary));
 }
 
+.is-selected {
+  border-color: var(--main-background);
+  background: linear-gradient(var(--main-background) 0 0) padding-box,
+    linear-gradient(180deg, rgba(var(--primary), 0.4), rgb(var(--primary)), rgba(var(--primary), 0.4)) border-box;
+  /* linear-gradient(180deg, red, white, red) border-box; */
+  border-width: 2px;
+  border-style: dashed;
+
+}
+
 /* Usual appearance */
 .material-card-background {
   display: grid;
@@ -83,6 +159,7 @@ watch(() => props.isAvailableForExchange, (newValue) => {
   grid-template-columns: 100%;
   grid-template-rows: auto 35%;
   background-color: var(--secondary-background);
+  /* background-color: var(--secondary-background); */
 }
 
 /* Usual appearance */
@@ -93,10 +170,30 @@ watch(() => props.isAvailableForExchange, (newValue) => {
   font-size: 16pt;
   font-weight: bold;
   grid-template-rows: auto 35%;
+  z-index: 1;
+}
+
+/* Sets layout for MaterialCard content area - content above and material type icon below */
+.material-card-content-wrapper {
+  display: grid;
+
+}
+
+.material-card-content-wrapper>* {
+  grid-row: 1;
+  grid-column: 1;
+}
+
+.material-card-content-wrapper>.material-type {
+  width: 10.6rem;
+  justify-self: center;
+  align-self: center;
+  filter: brightness(.2);
+  z-index: 0;
 }
 
 /* Usual appearance */
-.GradeIcon {
+.grade-icon {
   width: 2.5rem;
   align-self: flex-end;
   margin-bottom: 4px;
@@ -123,8 +220,13 @@ watch(() => props.isAvailableForExchange, (newValue) => {
 }
 
 /* 'Exchange unavailable' appearance */
-.material-card-red .GradeIcon {
+.material-card-red .grade-icon {
   fill: var(--secondary-red-text);
+}
+
+/* 'Exchange unavailable' appearance for `content` background icon */
+.material-card-red .material-type {
+  fill: rgba(var(--primary-red));
 }
 
 /* 'Exchange unavailable' appearance */
@@ -141,6 +243,12 @@ watch(() => props.isAvailableForExchange, (newValue) => {
 .material-card:hover .material-card-background {
   background-color: var(--primary-background);
   color: var(--black);
+  box-shadow: 0 0 70px rgba(var(--primary), 0.3);
+}
+
+/* Usual:HOVER appearance for `content` background icon */
+.material-card:hover .material-type {
+  fill: rgba(var(--primary), 0.12);
 }
 
 /* Usual:HOVER appearance */
@@ -149,8 +257,8 @@ watch(() => props.isAvailableForExchange, (newValue) => {
 }
 
 /* Usual:HOVER and 'Exchange unavailable':HOVER appearance */
-.material-card:hover .GradeIcon,
-.material-card-red:hover .GradeIcon {
+.material-card:hover .grade-icon,
+.material-card-red:hover .grade-icon {
   fill: black;
 }
 
@@ -158,11 +266,13 @@ watch(() => props.isAvailableForExchange, (newValue) => {
 .material-card-red:hover .material-card-background {
   background-color: var(--primary-red-background);
   color: var(--black);
+  box-shadow: 0 0 70px rgba(var(--primary-red), 0.3);
+
 }
 
 /* 'Exchange unavailable':HOVER appearance */
 .material-card-red:hover .material-card-title {
-  background-color: var(--primary-red);
+  background-color: rgb(var(--primary-red));
   color: var(--black);
 }
 </style>
